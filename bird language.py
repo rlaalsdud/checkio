6624c5755ca468fe71085f3d13b6231fb6506756
rlaalsdud <33623112+rlaalsdud@users.noreply.github.com>
2019-03-20 15:09:04 +0900

    checkio - home
    bird language

---
+++ bird language.py
@@ -0,0 +1,22 @@
+VOWELS = "aeiouy"
+
+def translate(phrase):
+    human = []
+    i = 0
+    while i < len(phrase):
+         human.append(phrase[i])
+         if phrase[i] in VOWELS:
+             i += 3
+         elif phrase[i] == ' ':
+             i += 1
+         else:
+             i += 2
+    return ''.join(human)
+        
+
+if __name__ == '__main__':
+    #These "asserts" using only for self-checking and not necessary for auto-testing
+    assert translate("hieeelalaooo") == "hello", "Hi!"
+    assert translate("hoooowe yyyooouuu duoooiiine") == "how you doin", "Joey?"
+    assert translate("aaa bo cy da eee fe") == "a b c d e f", "Alphabet"
+    assert translate("sooooso aaaaaaaaa") == "sos aaa", "Mayday, mayday"
