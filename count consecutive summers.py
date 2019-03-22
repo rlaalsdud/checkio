ff6c7411b3454f3c63cda34220ba2e04e925fe37
min yeong <33623112+rlaalsdud@users.noreply.github.com>
2019-03-22 16:10:52 +0900

    checkio - electronic-station
    count consecutive summers

--- count consecutive summers.py
+++ count consecutive summers.py
@@ -19,7 +19,7 @@ def count_consecutive_summers(num):
 if __name__ == '__main__':
     print("Example:")
     print(count_consecutive_summers(42))
-
+ 
     # These "asserts" are used for self-checking and not for an auto-testing
     assert count_consecutive_summers(42) == 4
     assert count_consecutive_summers(99) == 6
