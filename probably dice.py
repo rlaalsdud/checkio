831e944ddaae8edf80db378dddc601537b453731
min yeong <33623112+rlaalsdud@users.noreply.github.com>
2019-03-26 00:21:55 +0900

    checkio - electronic station
    probably dice

---
+++ probably dice.py
@@ -0,0 +1,31 @@
+import math
+def probability(dice_number, sides, target):
+    def nCr(n, r):
+        f = math.factorial
+        return f(n)/f(r)/f(n-r)
+        
+    if dice_number > target or dice_number*sides < target:
+        return 0
+    prob = (1/sides)**dice_number
+    max = (target-dice_number)/sides
+    sum = 0
+    for i in range(int(max)+1):
+        sum += (-1) ** i * nCr(dice_number, i) * nCr(target-sides*i-1, dice_number-1)
+        
+    
+    return prob*sum
+   
+
+if __name__ == '__main__':
+    #These are only used for self-checking and are not necessary for auto-testing
+    def almost_equal(checked, correct, significant_digits=4):
+        precision = 0.1 ** significant_digits
+        return correct - precision < checked < correct + precision
+        
+    assert(almost_equal(probability(2, 6, 3), 0.0556)), "Basic example"
+    assert(almost_equal(probability(2, 6, 4), 0.0833)), "More points"
+    assert(almost_equal(probability(2, 6, 7), 0.1667)), "Maximum for two 6-sided dice"
+    assert(almost_equal(probability(2, 3, 5), 0.2222)), "Small dice"
+    assert(almost_equal(probability(2, 3, 7), 0.0000)), "Never!"
+    assert(almost_equal(probability(3, 6, 7), 0.0694)), "Three dice"
+    assert(almost_equal(probability(10, 10, 50), 0.0375)), "Many dice, many sides"
