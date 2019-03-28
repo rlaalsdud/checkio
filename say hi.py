5110ce065611d1a7a14c355c0b46fd45bc04e99d
min yeong <33623112+rlaalsdud@users.noreply.github.com>
2019-03-28 14:54:44 +0900

    checkio - elementary
     say hi(java script)

--- say hi.py
+++ say hi.py
@@ -1,12 +1,13 @@
-# 1. on CheckiO your solution should be a function
-# 2. the function should return the right answer, not print it.
+"use strict";
 
-def say_hi(name: str, age: int) -> str:
-    # your code here
-    return "Hi. My name is {} and I'm {} years old".format(name, age)
+function sayHi(name, age) {
+    return "Hi. My name is " + name + " and I'm " +age + " years old";
+}
 
-if __name__ == '__main__':
-    #These "asserts" using only for self-checking and not necessary for auto-testing
-    assert say_hi("Alex", 32) == "Hi. My name is Alex and I'm 32 years old", "First"
-    assert say_hi("Frank", 68) == "Hi. My name is Frank and I'm 68 years old", "Second"
-    print('Done. Time to Check.')
+var assert = require('assert');
+
+if (!global.is_checking) {
+    assert.equal(sayHi("Alex", 32), "Hi. My name is Alex and I'm 32 years old")
+    assert.equal(sayHi("Frank", 68), "Hi. My name is Frank and I'm 68 years old")
+    console.log("Coding complete? Click 'Check' to review your tests and earn cool rewards!");
+}
